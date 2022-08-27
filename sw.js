ba9e07c334d3be92968fa631827af2344a17b825
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2022-08-27 12:07:22 +0100

    Implement advanced caching techniques

--- sw.js
+++ sw.js
@@ -10,26 +10,52 @@ const coreAssets = [
 ];
 
 async function preCache() {
-  const cache = await caches.open("app");
+  const cache = await caches.open("core");
 
   for (const asset of coreAssets) {
     cache.add(new Request(asset));
   }
 }
 
-async function networkFirst(request) {
+async function cacheOnRequest(cacheName, request, response) {
+  const copy = response.clone();
+  const cache = await caches.open(cacheName);
+  cache.put(request, copy);
+}
+
+async function networkFirst(event, request) {
+  const path = new URL(request.url).pathname.slice(1);
+
   try {
     const response = await fetch(request);
+
+    if (path && !coreAssets.includes(path)) {
+      event.waitUntil(cacheOnRequest("pages", request, response));
+    }
+
     return response;
   } catch {
     const response = await caches.match(request);
-    return response || caches.match("offline.html");
+    if (response) return response;
+
+    if (!path) return caches.match("index.html");
+
+    return caches.match("offline.html");
   }
 }
 
-async function offlineFirst(request) {
-  const response = await caches.match(request);
-  return response || fetch(request);
+async function offlineFirst(event, request) {
+  let response = await caches.match(request);
+  if (response) return response;
+
+  const header = request.headers.get("Accept");
+  response = await fetch(request);
+
+  if (header.includes("image")) {
+    event.waitUntil(cacheOnRequest("img", request, response));
+  }
+
+  return response;
 }
 
 self.addEventListener("install", function (event) {
@@ -47,12 +73,14 @@ self.addEventListener("fetch", function (event) {
   const header = request.headers.get("Accept");
 
   if (header.includes("text/html")) {
-    event.respondWith(networkFirst(request));
+    event.respondWith(networkFirst(event, request));
     return;
   }
 
-  if (header.includes("text/css") || header.includes("text/javascript")) {
-    event.respondWith(offlineFirst(request));
+  const offlineTypes = ["image", "text/css", "text/javascript"];
+
+  if (offlineTypes.some((type) => header.includes(type))) {
+    event.respondWith(offlineFirst(event, request));
     return;
   }
 });
